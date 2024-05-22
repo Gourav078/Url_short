5e7cb15d049428a1b4eda2cbf660460f052d1c31
Gourav Mitra <97431146+Gourav078@users.noreply.github.com>
2024-05-22 15:45:28 +0530

    Update user.js

--- URL_short/models/user.js
+++ URL_short/models/user.js
@@ -1,11 +1,7 @@
 import mongoose, { Schema, models } from "mongoose";
 
 const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      require: true,
-    },
+
     email: {
       type: String,
       require: true,
